d06e903467173d5547ca32773d0b6ddb4a95a2e3
lsaggu <lsaggu@indiana.edu>
2014-01-29 14:32:38 -0500

    Saggu first push

---
+++ updateCouchDB.py
@@ -0,0 +1,61 @@
+import couchdb
+from couchdb import *
+import json
+import tarfile
+
+from fileExplorer import fileExplorer
+
+
+# Provide commentary!
+
+def updateDB():
+#   Connect to server - no args means local connection
+    couch = couchdb.Server()
+#   Commentary:
+    print "Connected to CouchDB"
+#   Ask for database to connect to or create:
+    userInput = raw_input("Enter Database: ")
+    if userInput in couch: #Checks if entered db exists on server
+        db = couch[userInput]
+    else:
+        db = couch.create(userInput)
+    print userInput + " database opened."
+    
+    sample = tarfile.open("C://Users/Saggu/Downloads/sample.tar")
+    sampleFiles = sample.getmembers()
+
+    for file in sampleFiles:
+        #Extract file from tarfile
+        exFile = sample.extractfile(file)
+        #Deserialize the extracted ExFileObject into JSON object
+        document = json.load(exFile)
+
+        print "Adding document " + document.get("id")
+        
+        #Create '_id' field and set it equal to 'id' field in json document
+        document["_id"] = document.get("id")
+        #Add document to specified CouchDB db
+        _id, _rev = db.save(document)
+        print "Document %s added succesfully" %_id
+
+#   Useful for parsing through JSON docs
+#    for key, value in doc.iteritems():
+#        print key, value
+
+    print "Success"
+
+
+def main():
+    updateDB()
+
+#    print "Sup?"
+
+    
+if __name__ == "__main__":
+    main()
+
+
+#SOURCES
+    #docs.python.org
+    #philpapers.py by Jaimie Murdock
+    #StackOverflow
